7ac4effb26c3ca48813f657ae0b8ed3cf833e1a9
Sergejs Terentjevs <sergejs.terentjevs@gmail.com>
2022-11-26 15:22:59 +0200

    Update Flip.hs

--- Flip.hs
+++ Flip.hs
@@ -1,7 +1,7 @@
 import Control.Monad (ap)
 
 f :: (a1 -> a2) -> (b1 -> b2) -> (a1, b1) -> (a2, b2)
-f = flip flip snd . (ap .) . flip flip fst . ((.) .) . flip . (((.) . (,)) .)
+f =  flip flip snd . (ap .) . flip flip fst . ((.) .) . flip . (((.) . (,)) .)
 
 -- f (+1) ("foo" ++) (2, "asd")
 -- (3,"fooasd")
