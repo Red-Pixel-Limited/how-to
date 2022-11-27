14828203fee2d87a19238d5a12a4e5e1f438129e
Sergejs Terentjevs <sergejs.terentjevs@gmail.com>
2022-11-27 16:27:31 +0200

    Update Curried.scala

--- currying/Curried.scala
+++ currying/Curried.scala
@@ -3,7 +3,7 @@ object Curried {
   // given
   val f1: (Int, Int) => Int    = (a, b) => a + b
   val f2: Int => String        = _.toString
-  // function compose
+  // function composition
   val f3: (Int, Int) => String = (a, b) => f2.compose(f1.curried(a))(b)
   val f4: (Int, Int) => String = (a, b) => f1.curried(a).andThen(f2)(b)
 
